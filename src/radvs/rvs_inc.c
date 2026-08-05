1e7a445718b8c1fab0aeb9f5751fd6e44ffcfa04
Nikita Smith <git.nikita.smith@gmail.com>
2026-08-04 18:25:23 -0700

    whole engine include file

---
+++ src/radvs/rvs_inc.c
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "radvs/rvs_async.c"
+#include "radvs/rvs_request.c"
+#include "radvs/rvs_entity.c"
+#include "radvs/rvs_scheduler.c"
+#include "radvs/rvs_demon.c"
+#include "radvs/rvs_engine.c"
+
